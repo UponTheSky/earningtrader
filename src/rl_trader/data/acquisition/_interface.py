fe8f33e9c11c88ff39b486862e6d56ae6dd968b2
UponTheSky <sailing.to.orbis@gmail.com>
2023-08-06 13:59:24 +0900

    add: interfaces
    add a simple history data fetcher
    
    add: unit test for yfinance adapter

---
+++ src/rl_trader/data/acquisition/_interface.py
@@ -0,0 +1,26 @@
+from abc import ABC, abstractmethod
+
+import pandas as pd
+
+from rl_trader.interface import ComponentBaseInterface
+
+
+class DataFetcherInterface(ComponentBaseInterface, ABC):
+    """
+    The interface for data fetchers.
+    """
+
+    @abstractmethod
+    def fetch_history_data(self, *, ticker: str, period: str) -> pd.DataFrame:
+        """
+        Fetch the data according to a given list of options.
+        """
+        ...
+
+    @abstractmethod
+    def _log(self, *, message: str, level: str) -> None:
+        ...
+
+    @abstractmethod
+    def __repr__(self) -> str:
+        super().__repr__()
---
+++ src/rl_trader/data/acquisition/yfinance.py
@@ -0,0 +1,39 @@
+from typing import Literal
+import logging  # TODO: replace with the custom logger
+
+import yfinance as yf
+
+import pandas as pd
+
+from ._interface import DataFetcherInterface
+
+
+class YFinanceFetcher(DataFetcherInterface):
+    """
+    An adapter class for the yfinance package(https://github.com/ranaroussi/yfinance).
+
+    After investing some time on this open source, we may switch to use the original
+    APIs directly: see https://financeapi.net/.
+    """
+
+    _logger: logging.Logger  # TODO: replace with the custom logger
+
+    def __init__(self) -> None:
+        # TODO: add custom logger
+        self._logger = logging.getLogger(self.__class__.__name__)
+
+    def fetch_history_data(self, *, ticker: str, period: Literal["1d"]) -> pd.DataFrame:
+        try:
+            return yf.Ticker(ticker).history(period=period)
+
+        except Exception as error:
+            self._log(message=str(error))
+            raise
+
+    def _log(self, *, message: str, level: str) -> None:
+        # TODO: use the custom logger
+        return self._logger.info(message)
+
+    def __repr__(self) -> str:
+        # TODO: change this after more elaborations
+        return f"Component '{self.__class__.__name__}'"
