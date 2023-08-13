87d6d3f4b127d9a5901eaec096c5a2c3825f9a34
UponTheSky <sailing.to.orbis@gmail.com>
2023-08-13 18:11:05 +0900

    add: model implementation of the simple bernoulli thompson sampling

--- src/rl_trader/model/_interface.py
+++ src/rl_trader/model/_interface.py
@@ -1,12 +1,9 @@
 from __future__ import annotations
 
-from typing import Mapping, Any, Sequence
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
-from enum import Enum
 
 from rl_trader.common.interface import ComponentBaseInterface
-from rl_trader.storage import StorageInterface
 
 
 class RLTradingModelInterface(ComponentBaseInterface, ABC):
@@ -14,33 +11,16 @@ class RLTradingModelInterface(ComponentBaseInterface, ABC):
     The interface for RL trading models.
     """
 
-    @abstractmethod
-    def load_parameters(self, *, storage: StorageInterface, key: str) -> None:
-        """
-        Load the parameters of the current model.
-        """
+    _state: State
+
+    @property
+    def state(self) -> State:
         ...
 
     @abstractmethod
-    def set_parameters(self, *, parameters: Mapping[str, Any]) -> None:
+    def take_action(self) -> Action:
         """
-        Set the parameters of the current model.
-
-        This is either for setting new ones or updating the existing ones.
-        """
-        ...
-
-    @abstractmethod
-    def save_parameters(self, *, storage: StorageInterface, key: str) -> None:
-        """
-        Save the parameters of the current model into the storage.
-        """
-        ...
-
-    @abstractmethod
-    def take_action(self, *, states: Sequence[State]) -> Action:
-        """
-        Take an action based on the current parameters and the states given.
+        Take an action based on the current parameters and the state given.
         """
         ...
 
@@ -51,8 +31,15 @@ class RLTradingModelInterface(ComponentBaseInterface, ABC):
         """
         ...
 
+    @abstractmethod
+    def __repr__(self) -> str:
+        ...
 
-@dataclass
+    @abstractmethod
+    def _log(self) -> None:
+        ...
+
+
 class State(ABC):
     """
     A representation of a single unit of the current entire state.
@@ -61,16 +48,19 @@ class State(ABC):
     ...
 
 
-class Action(Enum, ABC):
+@dataclass(frozen=True)
+class Action(ABC):
     """
-    The enumeration of the actions to be taken.
+    Actions to be taken by the algorithm.
     """
 
     ...
 
 
-@dataclass
+@dataclass(frozen=True)
 class Reward(ABC):
     """
     The value of the reward earned as the result of the taken action.
     """
+
+    ...
